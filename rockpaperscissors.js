2ea9b53b53824d765dfda07ed2cf4896e9053000
Ivan Khoo <ivanakhoo@gmail.com>
2024-08-02 14:45:39 -0400

    UI Complete

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -50,25 +50,57 @@ function playRound(humanChoice, computerChoice) {
 }
 
 
-function playGame() {
-    let humanSel;
-    let compSel;
+// function playGame() {
+//     let humanSel;
+//     let compSel;
 
-    for (let i = 0; i < 5; i++) {
-        humanSel = getHumanChoice();
-        compSel = getComputerChoice();
-        console.log(playRound(humanSel, compSel));
-    }
+//     for (let i = 0; i < 5; i++) {
+//         humanSel = getHumanChoice();
+//         compSel = getComputerChoice();
+//         console.log(playRound(humanSel, compSel));
+//     }
 
-    return "Human Score: " + humanScore + " vs Computer Score: " + computerScore;
-}
+//     return "Human Score: " + humanScore + " vs Computer Score: " + computerScore;
+// }
 
-const btn = document.querySelectorAll("btn");
-btn.addEventListener("click", () => {
-    // Need to change
-    let choice = "ROCK";
-    // Step 2 Still
-    playRound(choice, getComputerChoice());
-})
+const container = document.querySelector("#container");
+const scoreBoard = document.createElement("p");
+scoreBoard.setAttribute("style", "color: blue; background: grey;");
+container.appendChild(scoreBoard);
+const log = document.createElement("p");
+container.appendChild(log);
+const winnerBoard = document.createElement("h1");
+container.appendChild(winnerBoard);
 
-// console.log(playGame());
+
+
+const buttons = document.querySelectorAll("button");
+
+var endGame = false;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (endGame) return;
+        
+        const recentMatch = playRound(button.id, getComputerChoice());
+        log.textContent = recentMatch; // Log the result of the round
+
+        scoreBoard.textContent = "My Score: " + humanScore + " vs Computer Score: " + computerScore;
+
+        if (humanScore >= 10 || computerScore >= 10) {
+            endGame = true;
+            if (humanScore > computerScore) {
+                winnerBoard.textContent = "You Win!";
+            } else {
+                winnerBoard.textContent = "CPU Wins!";
+            }
+        }
+    });
+});
+
+
+
+
+ 
+
+
